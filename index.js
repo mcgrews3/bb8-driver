3ed59952a090e6e9dd70cd98821afff846dcdbd1
jdmcgrew <justin.d.mcgrew@intel.com>
2015-09-07 17:29:26 -0700

    Progress on BLE info

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var bb8UUID = '3d83fa5f2fa943cc811c2907c050f9d0';
+bb8ServiceUUID = '22bb746f2ba075542d6f726568705327';
 var chargeUUID = 'e2822f5b0a4948bcaa16c28b7b1ebade';
 var Cylon = require('cylon');
 var cylonBLE = require('cylon-ble');
@@ -34,17 +35,20 @@ function discoverRobots() {
 						p.uuid,
 						p.rssi
 					].join(" | ") + "\n");
+					console.log(p);
 				}
 			});
 		}
 	}).start();
 }
 
-function readCharacteristics() {
+function readCharacteristic() {
+	var Cylon = require("cylon");
+
 	Cylon.robot({
 		connections: {
 			bluetooth: {
-				adaptor: "central", uuid: chargeUUID,
+				adaptor: "central", uuid: bb8UUID,
 				module: "cylon-ble"
 			}
 		},
@@ -52,7 +56,8 @@ function readCharacteristics() {
 		devices: {
 			wiced: {
 				driver: "ble-characteristic",
-				serviceId: "180f", characteristicId: "2a19",
+				serviceId: bb8ServiceUUID,
+				characteristicId: bb8UUID,
 				connection: "bluetooth"
 			}
 		},
@@ -66,4 +71,116 @@ function readCharacteristics() {
 	}).start();
 }
 
-readCharacteristics();
+function genericAccess() {
+	Cylon.robot({
+		connections: {
+			bluetooth: {
+				adaptor: "central",
+				uuid: bb8UUID,
+				module: "cylon-ble"
+			}
+		},
+
+		devices: {
+			generic: {
+				driver: "ble-generic-access"
+			}
+		},
+
+		work: function (my) {
+			console.log(my);
+			my.generic.getDeviceName(function (err, data) {
+				if (err) { return console.error("Error: ", err); }
+				console.log("Name: ", data);
+			});
+		}
+	}).start();
+}
+
+function roll() {
+	Cylon.robot({
+		connections: {
+			bluetooth: { adaptor: "central", uuid: bb8UUID, module: "cylon-ble" }
+		},
+
+		devices: {
+			ollie: { driver: "ollie" }
+		},
+
+		work: function (my) {
+			my.ollie.wake(function () {
+				every((1).second(), function () {
+					my.ollie.setRGB(Math.floor(Math.random() * 100000));
+				});
+			});
+		}
+	}).start();
+}
+
+function getDeviceInfo() {
+	var Cylon = require("cylon");
+
+	Cylon.robot({
+		connections: {
+			bluetooth: { adaptor: "central", uuid: bb8UUID, module: "cylon-ble" }
+		},
+
+		devices: {
+			deviceInfo: { driver: "ble-device-information" }
+		},
+
+		work: function (my) {
+			console.log(my.deviceInfo);
+			my.deviceInfo.getManufacturerName(function (err, data) {
+				if (err) {
+					console.log("error: ", err);
+					return;
+				}
+
+				console.log("data: ", data);
+			});
+		}
+	}).start();
+}
+
+function connect() {
+	Cylon.robot({
+		connections: {
+			bluetooth: { adaptor: "central", module: __dirname + "/node_modules/cylon-ble" }
+		},
+
+		connectBLE: function (peripheral) {
+			if (this.connected) { return; }
+			console.log(peripheral);
+
+			this.bluetooth.connectPeripheral(peripheral.uuid, peripheral, function () {
+				console.log(peripheral.advertisement.localName, peripheral.uuid);
+				this.connected = true;
+				this.device("blething",
+					{ connection: "bluetooth", driver: "ble-device-information" });
+				this.devices.blething.getManufacturerName(function (err, data) {
+					if (err) {
+						console.log("error: ", err);
+						return;
+					}
+					console.log("data: ", data);
+				});
+			}.bind(this));
+		},
+
+		work: function (my) {
+			this.connected = false;
+
+			my.bluetooth.on("discover", function (peripheral) {
+				my.connectBLE(peripheral);
+			});
+		}
+	}).start();
+}
+
+//getDeviceInfo();
+//genericAccess();
+//readCharacteristic();
+//discoverRobots();
+//roll();
+connect();
